ced830571163acf9b3ac5b688fdce2bcf1569a90
arthor_cui <arthor.cui@gmail.com>
2013-07-01 19:47:43 +0800

    add dig Db method

--- src/Domain/Service/AppStoresDataService.cs
+++ src/Domain/Service/AppStoresDataService.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Common.Const;
+using Model.AppStores;
 using SubSonic.Oracle.Repository;
+using TYD.Logging.Models;
 
 namespace Domain.Service
 {
@@ -14,6 +16,41 @@ namespace Domain.Service
         public AppStoresDataService()
         {
             Repository = new SimpleRepository(ConnectionStrings.Key_APPSTORES_ORACLE_LOG_STRING, SimpleRepositoryOptions.None);
+        }
+
+        public void GetData(long startId, long endId)
+        {
+            var dataList = Repository.Find<CommunicationLog>(x => x.Id > startId && x.Id <= endId).ToList();
+        }
+
+        public void DataFactory()
+        {
+            var measure = 1000;
+
+            for (int i = 0; i < 10; i++)
+            {
+                var data_K_list = Repository.Find<CommunicationLog>(x => x.Id > i * measure && x.Id <= (i + 1) * measure).ToList();
+                if (data_K_list != null)
+                {
+                    HandleData(data_K_list);
+                }
+
+            }
+
+
+
+        }
+
+
+        public void HandleData(List<CommunicationLog> dataList)
+        {
+            foreach (var item in dataList)
+            {
+                CommunicationLog result = new DigCommunicationLog();
+                result = item;
+            }
+
+
         }
 
 
---
+++ src/Model/AppStores/DigCommunicationLog.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SubSonic.Oracle.SqlGeneration.Schema;
+using TYD.Logging.Models;
+
+namespace Model.AppStores
+{
+    [Serializable]
+    [DailyPartitionSetting("CREATE_TIME", "PT_", "DIGCOMMU_")]
+    [SubSonicTableNameOverride("DIG_COMMUNICATION_LOG")]
+    public class DigCommunicationLog : CommunicationLog
+    {
+        [SubSonicColumnNameOverride("TIMES")]
+        public int Times { get; set; }
+
+        [SubSonicColumnNameOverride("YEAR")]
+        public int Year { get; set; }
+
+        [SubSonicColumnNameOverride("MONTH")]
+        public int Month { get; set; }
+
+        [SubSonicColumnNameOverride("DAY")]
+        public int Day { get; set; }
+
+    }
+}
